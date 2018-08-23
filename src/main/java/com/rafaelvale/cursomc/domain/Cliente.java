3c8f261e5b56dff22b7b81a7f2c43e5224aebb75
Rafel Vale <Rafael.Vale@rcky.com.br>
2018-08-23 14:41:48 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/rafaelvale/cursomc/domain/Cliente.java
+++ src/main/java/com/rafaelvale/cursomc/domain/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -123,6 +125,16 @@ public class Cliente implements Serializable {
 	}
 
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -148,7 +160,7 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 	
 	
 
---
+++ src/main/java/com/rafaelvale/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,42 @@
+package com.rafaelvale.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import com.rafaelvale.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento{
+		private static final long serialVersionUID = 1L;
+	private Integer numeroDeParcelas ;
+
+	public PagamentoComCartao() {
+		
+	}
+
+
+	
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	
+	
+	
+	
+
+}
