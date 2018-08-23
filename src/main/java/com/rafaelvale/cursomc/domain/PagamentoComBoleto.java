0b9ef5d1b04254c70a92faf56ff8baa4feb44317
Rafel Vale <Rafael.Vale@rcky.com.br>
2018-08-23 15:44:28 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/rafaelvale/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/rafaelvale/cursomc/domain/PagamentoComBoleto.java
@@ -5,13 +5,16 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rafaelvale.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 		private static final long serialVersionUID = 1L;
-	private Date dataVencimento;
-	private Date dataPagamento;
+		@JsonFormat(pattern="dd/MM/yyyy")
+		private Date dataVencimento;
+		@JsonFormat(pattern="dd/MM/yyyy")
+		private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
 		
